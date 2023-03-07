deccc3d500265e12bd68ea9d8925b9e6eaccc7be
Ilham513 <ilham513@outlook.com>
2023-03-07 11:34:15 +0700

    first commit

---
+++ tugas 1/Tugas1_IlhamSetiaBhakti.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
+
+<style>
+.center {
+  margin: auto;
+  margin-top:100px;
+  width: 60%;
+  border: 3px solid #008bff;
+  padding: 10px;
+  font-family: 'Roboto', sans-serif;
+  background-color: white;
+}
+td{
+  padding: 2px 2px;
+}
+body {
+  background-color: #163a76;
+}
+</style>
+</head>
+<body>
+
+<div class="center">
+ <table style="text-align:left">
+  <tr>
+    <th>Nama</th>
+    <td>: Ilham Setia Bhakti</td>
+  </tr>
+  <tr>
+    <th>Sekolah/Lulusan</th>
+    <td>: SMAN 1 Tambun Utara</td>
+  </tr>
+  <tr>
+    <th>No WA</th>
+    <td>: 08998994858</td>
+  </tr>
+  <tr>
+    <th>Alamat</th>
+    <td>: Tambun Selatan - Bekasi</td>
+  </tr>
+  <tr>
+    <th>Alasan Mengikuti Pelatihan</th>
+    <td>: Untuk mendapatkan skill  dalam dunia pemrograman</td>
+  </tr>
+</table>
+</div>
+
+</body>
+</html>
