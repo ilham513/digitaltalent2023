5c0985ac1bd3dbf50c848e3998ba1596227e4391
Ilham513 <ilham513@outlook.com>
2023-03-07 17:51:46 +0700

    first commit

--- pertemuan 2/latihan8.php
+++ pertemuan 2/latihan8.php
@@ -21,16 +21,72 @@ td{
 body {
   background-color: #163a76;
 }
+button{
+  margin-top:10px;
+  padding:4px;
+  font-size: 14px;
+}
 </style>
 </head>
 <body>
 
 <div class="center">
-	<?php
-		$benar=true;
-		$salah=false;
+	<div style="text-align:center;margin-bottom:8px;"><b>Kalkulator Luas Persegi Panjang</b></div>
+	<form action="#" method="GET">
+		Masukan Panjang <input type="number" name="panjang"> </br>
+		Masukan Lebar <input type="number" name="lebar"> </br>
+		<button type="submit">Hitung Luas</button>
+	</form>
+	<p>
+	<?php 
+	if (isset($_GET['panjang'])) {
+		$panjang = $_GET['panjang'];
+		$lebar = $_GET['lebar'];
 		
+		echo "Luas Persegi Panjang adalah ". ($panjang * $lebar);
+	}
 	?>
+	</p>
+	
+	<hr/>
+
+	<div style="text-align:center;margin-bottom:8px;"><b>Kalkulator Luas Segitiga</b></div>
+	<form action="#" method="GET">
+		Masukan Alas <input type="number" name="alas"> </br>
+		Masukan Tinggi <input type="number" name="tinggi"> </br>
+		<button type="submit">Hitung Luas</button>
+	</form>
+	<p>
+	<?php 
+	if (isset($_GET['alas'])) {
+		$alas = $_GET['alas'];
+		$tinggi = $_GET['tinggi'];
+		
+		echo "Luas Segitiga adalah ". (0.5 * $alas * $tinggi);
+	}
+	?>
+	</p>
+	
+	<hr/>
+
+	<div style="text-align:center;margin-bottom:8px;"><b>Kalkulator Luas Lingkaran</b></div>
+	<form action="#" method="GET">
+		Masukan Jari-jari <input type="number" name="jari2"> </br>
+		<button type="submit">Hitung Luas</button>
+	</form>
+	<p>
+	<?php 
+	if (isset($_GET['jari2'])) {
+		$jari2 = $_GET['jari2'];
+		
+		echo "Luas Lingkaran adalah ". (3.14 * $jari2 * $jari2);
+	}
+	?>
+	</p>
+	
+	<hr/>
+
+	
 </div>
 
 </body>
