0ddb668065c1a0b9703ce7af23a5a32d20ba129a
ilham513 <ilham513@outlook.com>
2023-03-07 07:39:13 +0700

    database

---
+++ vsga102/lat4.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+table, th, td {
+  border: 1px solid black;
+  padding: 8px;
+  border-collapse: collapse;
+}
+
+table{
+  margin: auto;
+  width: 80%;
+}
+</style>
+</head>
+
+<body>
+
+ <table>
+  <tr>
+    <th colspan="2">Name</th>
+    <th>Age</th>
+  </tr>
+  <tr>
+    <td>Jill</td>
+    <td>Smith</td>
+    <td>43</td>
+  </tr>
+  <tr>
+    <td>Eve</td>
+    <td>Jackson</td>
+    <td>57</td>
+  </tr>
+</table> 
+
+<hr/>
+
+ <table>
+  <tr>
+    <th>Name</th>
+    <td>Jill</td>
+  </tr>
+  <tr>
+    <th rowspan="2">Phone</th>
+    <td>555-1234</td>
+  </tr>
+  <tr>
+    <td>555-8745</td>
+</tr>
+</table> 
+
+
+</body>
+</html>
